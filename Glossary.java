18df35d8911aa93174d78570ab2b420a516a95c5
The Chair <d3vin213@gmail.com>
2025-11-24 11:30:23 -0700

    v0.2

--- Glossary.java
+++ Glossary.java
@@ -1,5 +1,50 @@
 package comprehensive;
 
-public class Glossary {
+import java.util.Scanner;
+import java.util.TreeSet;
+import java.io.File;
+import java.io.FileNotFoundException;
 
+public class Glossary {
+	private TreeSet<Term> glossary;
+	
+	public Glossary(String filePath)
+	{
+		glossary = new TreeSet<Term>();
+		
+		readFile(filePath);
+	}
+	
+	
+	private void readFile(String filePath)
+	{
+		Scanner sc;
+		
+		try {
+			sc = new Scanner(new File(filePath));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		String[] data;
+		
+		while (sc.hasNextLine()) {
+			data = sc.nextLine().split("::");
+			
+			addTerm(data);
+		}
+
+		sc.close();
+	}
+	
+	public void addTerm(String[] data)
+	{
+		glossary.add(new Term(data));
+	}
+	
+	public boolean changeDef()
+	{
+		return false;
+	}
 }
--- Term.java
+++ Term.java
@@ -1,5 +1,26 @@
 package comprehensive;
 
-public class Term {
+import java.util.HashMap;
+import java.util.TreeSet;
 
+public class Term {
+	private String word;
+	
+	private HashMap<String, TreeSet<String>> definitions;
+	
+	
+	public Term(String[] data) {
+		this.word = data[0];
+		
+		definitions = new HashMap<String, TreeSet<String>>();
+		
+		add(data[1], data[2]);
+	}
+	
+	public boolean add(String pos, String def) {
+		if (!definitions.containsKey(pos))
+			definitions.put(pos, new TreeSet<String>());
+		
+		return definitions.get(pos).add(def);
+	}
 }
